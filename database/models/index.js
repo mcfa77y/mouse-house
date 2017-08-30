6b2d3a53a12368e294b237ac0b2f36df7401b316
joe <joe.lau@placecast.net>
2017-08-30 12:12:27 -0700

    more db url updates

--- database/models/index.js
+++ database/models/index.js
@@ -8,8 +8,8 @@ const env       = process.env.NODE_ENV || 'development';
 const config    = require(__dirname + '/../config/config.json')[env];
 const db        = {};
 let sequelize
-if (config.use_env_constiable) {
-  sequelize = new Sequelize(process.env[config.use_env_constiable]);
+if (process.env.NODE_ENV == 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
