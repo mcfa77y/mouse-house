c155797d19ed3287cec6e0c72cfe5e09750d5bc7
Joe Lau <mcfa77y@gmail.com>
2018-02-12 09:34:13 -0800

    breed-plug-pup-check init

--- public_src/javascripts/cs-breed-update.js
+++ public_src/javascripts/cs-breed-update.js
@@ -1,5 +1,13 @@
-import {setup_update_page_buttons} from './cs-model-common'
+import { setup_update_page_buttons } from './cs-model-common';
 
-$(function() {
-     setup_update_page_buttons('breed')
-})
+function setup_link_plug_pup_check_dates() {
+  $('#plug-date').change(() => {
+    const foo = $('#plug-date').val();
+    console.log(foo);
+  });
+}
+
+$(() => {
+  setup_update_page_buttons('breed');
+  setup_link_plug_pup_check_dates();
+});
