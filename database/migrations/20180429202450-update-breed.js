c082d00d8b6a0b04b63843dd0691ba3c4e4fe57d
Joe Lau <mcfa77y@gmail.com>
2018-05-08 10:13:23 -0700

    working on migration script to add fe/male id to breed

--- database/migrations/20180429202450-update-breed.js
+++ database/migrations/20180429202450-update-breed.js
@@ -19,12 +19,43 @@ module.exports = {
         }
     })
 
-    return Promise.all([add_male_id, add_female_id])
+    const drop_breed_mice_table = queryInterface.dropTable('Breed_Mice')
+    return Promise.all([add_male_id, add_female_id, drop_breed_mice_table])
   },
   down: function(queryInterface, Sequelize) {
     const remove_male_id = queryInterface.removeColumn('Breeds', 'male_id');
     const remove_female_id = queryInterface.removeColumn('Breeds', 'female_id');
+    const create_breed_mice = queryInterface.createTable('Breed_Mice', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      breed_id: {
+        type: Sequelize.INTEGER,
+        references: {
+              model: "Breeds",
+              key: "id"
+          }
+      },
+      mouse_id: {
+        type: Sequelize.INTEGER,
+        references: {
+              model: "Mice",
+              key: "id"
+          }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
 
-    return Promise.all([remove_male_id, remove_female_id]);
+    return Promise.all([remove_male_id, remove_female_id, create_breed_mice]);
   }
 };
