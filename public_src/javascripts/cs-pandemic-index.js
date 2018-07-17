6714152738a4e9f1b9659b50d08d60534392e8b9
Joe Lau <mcfa77y@gmail.com>
2018-07-17 09:45:48 -0700

    pandemic
    changed on change to on Add to help alleviate 2x adding

--- public_src/javascripts/cs-pandemic-index.js
+++ public_src/javascripts/cs-pandemic-index.js
@@ -96,7 +96,8 @@ $(() => {
             instances: 3
         },
     ]
-    console.log('pandemic index');
+    
+    
     const city_select = $('.city_select');
     const find_color = (city_name) => {
         return infection_card_list.find((x) => {
@@ -133,10 +134,8 @@ $(() => {
         for (var i = 0, l = lis.length; i < l; i++)
             lis[i].innerHTML = vals[i];
     }
-    const change_fn = (value_list) => {
-        if (!value_list.length) return;
-        const value = value_list[0];
-        city_select[0].selectize.clear();
+    const change_fn = (value) => {
+    
         const color = find_color(value);
         const group = `${color}-group`
         const button = `<button type="button" class="close" aria-label="Close">
@@ -144,16 +143,14 @@ $(() => {
 </button>`
         $(`#${group}`).append(`<li class="list-group-item" value="${value}">${value}${button}</li>`);
         sortUnorderedList(group);
-
+        city_select[0].selectize.clear();
     }
     $('#infect_button').click(() => {
         const city_name_list = $('.list-group-item').toArray().map(x => x.getAttribute('value'));
         const group_city_name_count = city_name_list.reduce((acc, cur) => {
             if (acc[cur] != null) {
-                console.log('found: ' + cur);
                 acc[cur] = acc[cur] + 1.0;
             } else {
-                console.log('new: ' + cur);
                 acc[cur] = 1.0;
             }
             return acc;
@@ -177,7 +174,7 @@ $(() => {
         searchField: 'city',
         options: infection_card_list,
         create: false,
-        onChange: change_fn,
+        onItemAdd: change_fn,
         hideSelected: false,
     });
 })
