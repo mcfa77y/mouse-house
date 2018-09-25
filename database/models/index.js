31c22681b54a0b384746aa4162cce1a339a1f35d
Joe Lau <mcfa77y@gmail.com>
2018-09-25 09:40:31 -0700

    updated packages
    added redis (sort of?)

--- database/models/index.js
+++ database/models/index.js
@@ -1,13 +1,18 @@
-
+const cacher = require('sequelize-redis-cache');
+const redis = require('redis');
 
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 
+const redis_url = process.env.REDIS_URL;
+const redis_client = redis.createClient(redis_url);
+
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
+const ONE_HOUR = 60 * 60;
 let sequelize;
 
 if (process.env.NODE_ENV === 'production') {
@@ -28,6 +33,9 @@ fs
   .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
+    db['cached_' + model.name] = cacher(sequelize, redis_client)
+      .model(model.name)
+      .ttl(ONE_HOUR);
   });
 
 Object.keys(db).forEach((modelName) => {
