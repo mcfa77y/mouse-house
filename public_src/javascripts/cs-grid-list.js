2a596a38526deeadcf25314b741369b7b68b1b57
Joe Lau <mcfa77y@gmail.com>
2018-12-14 17:00:26 -0800

    using more of the space
    container-fluid

--- public_src/javascripts/cs-grid-list.js
+++ public_src/javascripts/cs-grid-list.js
@@ -3,24 +3,24 @@ import { form_ids_vals } from './cs-form-helper';
 
 const zeroFill = require('zero-fill');
 
+const model_name = 'grid';
+const foo_this = (event, klass) => {
+    const clickedElement = $(event.target);
+    const targetElement = clickedElement.closest(klass);
+    return targetElement;
+};
 
-$(() => {
-    const submit_button = $('#submit');
+const setup_grid_cells = () => {
     const results = $('#results');
-    const model_name = 'grid';
-
     const error = (data) => {
         results.html(`error: ${data.error_message}`);
     };
-
-
     const create_card = (response) => {
         $('#image_row').append(`${response.data.html}`);
     };
 
     $(document).on('click', '.hover_cell', (event) => {
-        const clickedElement = $(event.target);
-        const targetElement = clickedElement.closest('.hover_cell');
+        const targetElement = foo_this(event, '.hover_cell');
         const index = zeroFill(3, targetElement.text());
         const dt = $.extend(form_ids_vals(`${model_name}-fields`), { index });
         Axios.post(`/${model_name}/card`, dt)
@@ -29,14 +29,30 @@ $(() => {
 
         // $(`#image_${index}`).toggleClass('d-none');
     });
+};
 
-    $(document).on('click', '.close-btn', function () {
-        $(this).closest('.card').fadeOut({ complete: function() { $(this).remove(); } });
+const setup_cards = () => {
+    $(document).on('click', '.close-btn', (event) => {
+        const targetElement = foo_this(event, '.close-btn');
+        targetElement.closest('.card').fadeOut({ complete() { $(this).remove(); } });
     });
-    $(document).on('click', '.card-img-top', function () {
-        const img_uri = $(this).closest('.card-img-top').attr('src');
-        window.open(img_uri,'Image');
+    $(document).on('click', '.card-img-top', (event) => {
+        const targetElement = foo_this(event, '.card-img-top');
+        const img_uri = targetElement.closest('.card-img-top').attr('src');
+        window.open(img_uri, 'Image');
     });
+};
+
+const setup_form = () => {
+    const submit_button = $('#submit');
+    const results = $('#results');
+
+
+    const error = (data) => {
+        results.html(`error: ${data.error_message}`);
+    };
+
+
     const create_table = (response) => {
         results.html(`${response.data.html}`);
         const table_options = {
@@ -49,11 +65,18 @@ $(() => {
 
     submit_button.click((e) => {
         e.preventDefault();
-        $('#collapseOne').collapse();
+        $('#exampleModal').modal('hide');
         $('#collapseTwo').collapse('show');
         const dt = form_ids_vals(`${model_name}-fields`);
         Axios.post(`/${model_name}/table`, dt)
             .then(create_table)
             .catch(error);
     });
+};
+
+$(() => {
+    setup_form();
+    setup_grid_cells();
+    setup_cards();
+    $('#exampleModal').modal('show');
 });
