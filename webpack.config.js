228eb875ced15e3976fed1db7b2fc1248b5b3ac9
Joe Lau <mcfa77y@gmail.com>
2019-07-19 16:58:52 -0700

    working on fixing webpack and upgraded to node 12

--- webpack.config.js
+++ webpack.config.js
@@ -112,6 +112,7 @@ module.exports = {
                 options: {
                     presets: ['env'],
                 },
+                exclude: /node_modules/,
             },
         },
         ],
