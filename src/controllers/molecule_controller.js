179a16e6bd498994b570ff55aa7639d80587e7f8
Ginkies <lau.joe.a@gmail.com>
2020-02-11 09:53:46 -0800

    grid viewer: init crc import

--- src/controllers/molecule_controller.js
+++ src/controllers/molecule_controller.js
@@ -176,29 +176,24 @@ class Molecule_Controller extends Base_Controller {
         limit, offset, columns, search, order,
     }) {
         const self = this;
-        let all_molecules;
-        let molecules;
+
         const where = this.build_where(columns);
         const include = this.build_include(columns);
         const order_by = this.build_order(columns, order);
-        const base_config = {
+        let config = {
             include,
             where,
             order: order_by,
         };
         if (limit > 0) {
-            const config = Object.assign(base_config, { limit, offset });
-            all_molecules = await Molecule.findAndCountAll(config).catch((error) => {
-                console.log(`error - find some molecule: ${error}`);
-                console.log(`error - find some molecule stack:\n ${error.stack}`);
-            });
-            molecules = all_molecules.rows.map((molecule) => self.pretty(molecule));
-        } else {
-            all_molecules = await Molecule.findAll(base_config).catch((error) => {
-                console.log(`error - find all molecule: ${error}`);
-            });
-            molecules = all_molecules.map((molecule) => self.pretty(molecule));
+            config = Object.assign(config, { limit, offset });
         }
+        const all_molecules = await Molecule.findAndCountAll(config).catch((error) => {
+            console.log(`error - find some molecule: ${error}`);
+            console.log(`error - find some molecule stack:\n ${error.stack}`);
+        });
+        const molecules = all_molecules.rows.map((molecule) => self.pretty(molecule));
+
         return {
             molecules,
             count: all_molecules.count,
